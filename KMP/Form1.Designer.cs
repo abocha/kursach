d0caf0c2c40d29dc61c42ce491cc08e648da1a72
Andrew Bochkarev <Andrew Bochkarev>
2017-09-28 19:30:10 +1000

    Добавьте файлы проекта.

---
+++ KMP/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace KMP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.inpPattern = new System.Windows.Forms.TextBox();
+            this.inpText = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "See result";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // inpPattern
+            // 
+            this.inpPattern.Location = new System.Drawing.Point(6, 19);
+            this.inpPattern.Name = "inpPattern";
+            this.inpPattern.Size = new System.Drawing.Size(100, 20);
+            this.inpPattern.TabIndex = 2;
+            this.inpPattern.TextChanged += new System.EventHandler(this.inpPattern_TextChanged);
+            // 
+            // inpText
+            // 
+            this.inpText.Location = new System.Drawing.Point(6, 19);
+            this.inpText.Name = "inpText";
+            this.inpText.Size = new System.Drawing.Size(100, 20);
+            this.inpText.TabIndex = 3;
+            this.inpText.TextChanged += new System.EventHandler(this.inpText_TextChanged);
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(113, 19);
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.Size = new System.Drawing.Size(100, 20);
+            this.output.TabIndex = 7;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.groupBox1);
+            this.flowLayoutPanel1.Controls.Add(this.groupBox2);
+            this.flowLayoutPanel1.Controls.Add(this.groupBox3);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(39, 43);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(253, 119);
+            this.flowLayoutPanel1.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.inpPattern);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(119, 50);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Substring";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.inpText);
+            this.groupBox2.Location = new System.Drawing.Point(128, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(119, 50);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "String";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.output);
+            this.groupBox3.Location = new System.Drawing.Point(3, 59);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(238, 54);
+            this.groupBox3.TabIndex = 11;
+            this.groupBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 429);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "KMP";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox inpPattern;
+        private System.Windows.Forms.TextBox inpText;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ KMP/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KMP
+{
+    public partial class Form1 : Form
+    {
+        public string ReturnValue1 { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            pattern = this.inpPattern.Text;
+            text = this.inpText.Text;
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(FindSubstring(pattern, text).ToString());
+            if (pattern.Length > 0 && text.Length > 0)
+                {
+                this.output.Text = FindSubstring(pattern, text).ToString();
+                }
+        }
+
+        string pattern, text;
+        //int result = FindSubstring(pattern, text);
+
+        static int[] GetPrefix(string s)
+        {
+            int[] result = new int[s.Length];
+            result[0] = 0;
+            int index = 0;
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                while (index >= 0 && s[index] != s[i]) { index--; }
+                index++;
+                result[i] = index;
+            }
+
+            return result;
+        }
+
+        static int FindSubstring(string pattern, string text)
+        {
+            int res = -1;
+            int[] pf = GetPrefix(pattern);
+            int index = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                while (index > 0 && pattern[index] != text[i]) { index = pf[index - 1]; }
+                if (pattern[index] == text[i]) index++;
+                if (index == pattern.Length)
+                {
+                    return res = i - index + 1;
+                }
+            }
+
+            return res;
+        }
+
+        private void inpPattern_TextChanged(object sender, EventArgs e)
+        {
+            pattern = this.inpPattern.Text;
+        }
+
+        private void output_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inpText_TextChanged(object sender, EventArgs e)
+        {
+            text = this.inpText.Text;
+        }
+    }
+}
