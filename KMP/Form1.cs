d2bf790847f5ac3e32a7cbfeafd75893cce83e3e
Andrew Bochkarev <abocha@ya.ru>
2017-10-25 05:16:00 +1000

    apost

--- KMP/Form1.cs
+++ KMP/Form1.cs
@@ -26,46 +26,90 @@ namespace KMP
             //MessageBox.Show(FindSubstring(pattern, text).ToString());
             if (pattern.Length > 0 && text.Length > 0)
                 {
-                this.output.Text = FindSubstring(pattern, text).ToString();
+                this.output.Text = AG(pattern, text).ToString();
                 }
         }
 
         string pattern, text;
         //int result = FindSubstring(pattern, text);
 
-        static int[] GetPrefix(string s)
-        {
-            int[] result = new int[s.Length];
-            result[0] = 0;
-            int index = 0;
+        private void getT(string x, int[] t)
+        { //функция, вычисляющая массив t для строки x
 
-            for (int i = 1; i < s.Length; i++)
+            int i = 0;
+            int j = -1;
+            t[0] = j;
+            while (i < x.Length-1)
             {
-                while (index >= 0 && s[index] != s[i]) { index--; }
-                index++;
-                result[i] = index;
+                while ((j > -1) && (x[i] != x[j]))
+                {
+                    j = t[j];
+                }
+            i++;
+            j++;
+            if (x[i] == x[j])
+            {
+                t[i] = t[j];
             }
-
-            return result;
+            else t[i] = j;
+            }
         }
 
-        static int FindSubstring(string pattern, string text)
-        {
-            int res = -1;
-            int[] pf = GetPrefix(pattern);
-            int index = 0;
+        public int AG(string xx, string y)
+        { //x — образец, y — текст
+            int l = new int();
+            int[] t = new int[xx.Length];
+            int v = new int();
 
-            for (int i = 0; i < text.Length; i++)
+            //этап предпосчета
+            getT(xx, t);
+            //вычисление значения l 
+            for (l = 1; xx[l - 1] == xx[l]; l++)
             {
-                while (index > 0 && pattern[index] != text[i]) { index = pf[index - 1]; }
-                if (pattern[index] == text[i]) index++;
-                if (index == pattern.Length)
+                if (l == xx.Length)
                 {
-                    return res = i - index + 1;
+                    l = 0;
                 }
             }
-
-            return res;
+            //этап поиска
+            int i = l;
+            int j = 0;
+            int k = 0;
+            while (j <= y.Length-1 - xx.Length-1)
+            {
+                while ((i < xx.Length-1) && (xx[i] == y[i + j]))
+                { // если x[i] = y[i + j]
+                    ++i;
+                }          // тогда следующая тройка (i + 1, j, k) 
+                if (i >= xx.Length-1)
+                {
+                    while ((k < l) && (xx[k] == y[j + k]))
+                    { // если k < l и x[k] = y[j + k]
+                        ++k;
+                    }                           // тогда следующая тройка (i, j, k + 1) 
+                    if (k >= l)
+                    {           // если k = l 
+                        v = j;
+                    }   // тогда найдена подстрока в позиции j
+                }
+                j += i - t[i];         // вычисляем новый сдвиг
+                if (i == l)
+                {
+                    k = Math.Max(0, k - 1);
+                } // если i = l и x[i] \neq y[i + j], тогда следующая тройка (l, j + 1, \max(0, k - 1))
+                else
+                   if (t[i] <= l)
+                { // если t[i] \leqslant l, тогда следующая тройка (l, i + j - t[i], \max(0, t[i]))
+                    k = Math.Max(0, t[i]);
+                    i = l;
+                }
+                else
+                {           // если t[i] > l, тогда следующая тройка (t[i], i + j - t[i], l)
+                    k = l;
+                    i = t[i];
+                }
+            }
+            return v;
         }
 
         private void inpPattern_TextChanged(object sender, EventArgs e)
