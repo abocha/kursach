be69edd806e307ec136d2332cb19d54f689c14b6
Andrew Bochkarev <abocha@ya.ru>
2017-10-25 10:07:06 +1000

    squence generation

--- KMP/Form1.Designer.cs
+++ KMP/Form1.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonResult = new System.Windows.Forms.Button();
             this.inpPattern = new System.Windows.Forms.TextBox();
             this.inpText = new System.Windows.Forms.TextBox();
             this.output = new System.Windows.Forms.TextBox();
@@ -36,21 +36,33 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.genError = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.genSubstrLength = new System.Windows.Forms.NumericUpDown();
+            this.genLength = new System.Windows.Forms.NumericUpDown();
+            this.genSearchType = new System.Windows.Forms.ComboBox();
             this.flowLayoutPanel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.genSubstrLength)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.genLength)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonResult
             // 
-            this.button1.Location = new System.Drawing.Point(32, 17);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "See result";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonResult.Location = new System.Drawing.Point(32, 17);
+            this.buttonResult.Name = "buttonResult";
+            this.buttonResult.Size = new System.Drawing.Size(75, 23);
+            this.buttonResult.TabIndex = 1;
+            this.buttonResult.Text = "See result";
+            this.buttonResult.UseVisualStyleBackColor = true;
+            this.buttonResult.Click += new System.EventHandler(this.buttonResult_Click);
             // 
             // inpPattern
             // 
@@ -64,7 +76,7 @@
             // 
             this.inpText.Location = new System.Drawing.Point(6, 19);
             this.inpText.Name = "inpText";
-            this.inpText.Size = new System.Drawing.Size(100, 20);
+            this.inpText.Size = new System.Drawing.Size(449, 20);
             this.inpText.TabIndex = 3;
             this.inpText.TextChanged += new System.EventHandler(this.inpText_TextChanged);
             // 
@@ -80,10 +92,9 @@
             // 
             this.flowLayoutPanel1.Controls.Add(this.groupBox1);
             this.flowLayoutPanel1.Controls.Add(this.groupBox2);
-            this.flowLayoutPanel1.Controls.Add(this.groupBox3);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(39, 43);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(253, 119);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(603, 119);
             this.flowLayoutPanel1.TabIndex = 8;
             // 
             // groupBox1
@@ -101,26 +112,122 @@
             this.groupBox2.Controls.Add(this.inpText);
             this.groupBox2.Location = new System.Drawing.Point(128, 3);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(119, 50);
+            this.groupBox2.Size = new System.Drawing.Size(461, 50);
             this.groupBox2.TabIndex = 10;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "String";
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.buttonResult);
             this.groupBox3.Controls.Add(this.output);
-            this.groupBox3.Location = new System.Drawing.Point(3, 59);
+            this.groupBox3.Location = new System.Drawing.Point(15, 71);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(238, 54);
+            this.groupBox3.Size = new System.Drawing.Size(235, 54);
             this.groupBox3.TabIndex = 11;
             this.groupBox3.TabStop = false;
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(282, 58);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(75, 23);
+            this.buttonGenerate.TabIndex = 9;
+            this.buttonGenerate.Text = "Generate";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.genError);
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Controls.Add(this.label1);
+            this.groupBox4.Controls.Add(this.genSubstrLength);
+            this.groupBox4.Controls.Add(this.genLength);
+            this.groupBox4.Controls.Add(this.genSearchType);
+            this.groupBox4.Controls.Add(this.buttonGenerate);
+            this.groupBox4.Location = new System.Drawing.Point(12, 137);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(603, 100);
+            this.groupBox4.TabIndex = 10;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Sequence Generation";
+            // 
+            // genError
+            // 
+            this.genError.AutoSize = true;
+            this.genError.Location = new System.Drawing.Point(379, 67);
+            this.genError.Name = "genError";
+            this.genError.Size = new System.Drawing.Size(0, 13);
+            this.genError.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(279, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Sequence Type";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Substring Length";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "String Length";
+            // 
+            // genSubstrLength
+            // 
+            this.genSubstrLength.Location = new System.Drawing.Point(129, 58);
+            this.genSubstrLength.Name = "genSubstrLength";
+            this.genSubstrLength.Size = new System.Drawing.Size(120, 20);
+            this.genSubstrLength.TabIndex = 16;
+            this.genSubstrLength.ValueChanged += new System.EventHandler(this.genSubstrLength_ValueChanged);
+            // 
+            // genLength
+            // 
+            this.genLength.Location = new System.Drawing.Point(129, 28);
+            this.genLength.Name = "genLength";
+            this.genLength.Size = new System.Drawing.Size(120, 20);
+            this.genLength.TabIndex = 15;
+            this.genLength.ValueChanged += new System.EventHandler(this.genLength_ValueChanged);
+            // 
+            // genSearchType
+            // 
+            this.genSearchType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.genSearchType.FormattingEnabled = true;
+            this.genSearchType.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.genSearchType.Items.AddRange(new object[] {
+            "Alphanumeric",
+            "Numeric",
+            "A, B, C"});
+            this.genSearchType.SelectedIndex = 2;
+            this.genSearchType.Location = new System.Drawing.Point(379, 27);
+            this.genSearchType.Name = "genSearchType";
+            this.genSearchType.Size = new System.Drawing.Size(121, 21);
+            this.genSearchType.TabIndex = 13;
+            this.genSearchType.Tag = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(814, 429);
+            this.ClientSize = new System.Drawing.Size(636, 259);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "Form1";
             this.Text = "KMP";
@@ -131,12 +238,16 @@
             this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.genSubstrLength)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.genLength)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonResult;
         private System.Windows.Forms.TextBox inpPattern;
         private System.Windows.Forms.TextBox inpText;
         private System.Windows.Forms.TextBox output;
@@ -144,6 +255,15 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ComboBox genSearchType;
+        private System.Windows.Forms.NumericUpDown genLength;
+        private System.Windows.Forms.NumericUpDown genSubstrLength;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label genError;
     }
 }
 
--- KMP/Form1.cs
+++ KMP/Form1.cs
@@ -21,7 +21,12 @@ namespace KMP
             text = this.inpText.Text;
         }
         
-        private void button1_Click(object sender, EventArgs e)
+        private int getSearchType()
+        {
+            return genSearchType.SelectedIndex;
+        }
+
+        private void buttonResult_Click(object sender, EventArgs e)
         {
             //MessageBox.Show(FindSubstring(pattern, text).ToString());
             if (pattern.Length > 0 && text.Length > 0)
@@ -29,9 +34,10 @@ namespace KMP
                 this.output.Text = FindSubstring(pattern, text).ToString();
                 }
         }
-
+        
         string pattern, text;
-        //int result = FindSubstring(pattern, text);
+        private int genLengthValue;
+        private int genSubstrLengthValue;
 
         static int[] GetPrefix(string s)
         {
@@ -64,23 +70,66 @@ namespace KMP
                     return res = i - index + 1;
                 }
             }
-
             return res;
+        }
+        
+        private static Random random = new Random();
+        public static string RandomString(int length, string chars)
+        {            
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            if (genSubstrLength.Value > genLength.Value)
+            {
+                genError.Text = "Substring Length must be less or equal to String Length";
+            }
+            else
+            {
+                genError.Text = "";
+                //string searchType = genSearchType.SelectedItem.ToString();
+                string chars = "";
+                switch (getSearchType())
+                {
+                    case 0:
+                        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                        inpPattern.Text = RandomString(genSubstrLengthValue, chars);
+                        inpText.Text = RandomString(genLengthValue, chars);
+                        break;
+                    case 1:
+                        chars = "0123456789";
+                        inpPattern.Text = RandomString(genSubstrLengthValue, chars);
+                        inpText.Text = RandomString(genLengthValue, chars);
+                        break;
+                    case 2:
+                        chars = "ABC";
+                        inpPattern.Text = RandomString(genSubstrLengthValue, chars);
+                        inpText.Text = RandomString(genLengthValue, chars);
+                        break;
+                }
+            }
+        }
+
+        private void genLength_ValueChanged(object sender, EventArgs e)
+        {
+            genLengthValue = Convert.ToInt32(genLength.Value);
+        }
+
+        private void genSubstrLength_ValueChanged(object sender, EventArgs e)
+        {
+            genSubstrLengthValue = Convert.ToInt32(genSubstrLength.Value);
+        }
+
+        private void inpText_TextChanged(object sender, EventArgs e)
+        {
+            text = inpText.Text;
         }
 
         private void inpPattern_TextChanged(object sender, EventArgs e)
         {
             pattern = this.inpPattern.Text;
-        }
-
-        private void output_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void inpText_TextChanged(object sender, EventArgs e)
-        {
-            text = this.inpText.Text;
         }
     }
 }
